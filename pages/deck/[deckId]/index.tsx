eeabac9d7d8628026dd63c989d89493e37d70c77
Ryan Pears <ryjen.pears@gmail.com>
2023-07-16 18:16:41 +0200

    poages updated

--- pages/deck/[deckId]/index.tsx
+++ pages/deck/[deckId]/index.tsx
@@ -5,19 +5,28 @@ const Deck = () => {
   const [deck, setDeck] = useState({} as any);
   const [isLoading, setLoading] = useState(false);
   const [characters, setCharacters] = useState(Array<any>);
+  const [allCharacters, setAllCharacters] = useState(Array<any>);
+  const [errorAlreadyExists, showErrorAlreadyExists] = useState(false);
 
   const router = useRouter();
   const { deckId } = router.query;
+  const url = `/api/deck/${deckId}`;
+
+  useEffect(() => {
+    fetch("/api/character")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllCharacters(data);
+      });
+  }, []);
 
   useEffect(() => {
     setLoading(true);
 
     async function getDeck() {
-      const endpoint = `/api/deck/${deckId}`;
-      console.log(deckId);
-      const response = await fetch(endpoint);
+      const response = await fetch(url);
       const result = await response.json();
-
+      console.log(result.characters);
       setDeck(result);
       setCharacters(result.characters); // set characters from deck
       setLoading(false);
@@ -26,7 +35,7 @@ const Deck = () => {
     if (deckId) {
       getDeck();
     }
-  }, [deckId]);
+  }, [deckId, url]);
 
   const handleDeleteDeck = () => {
     const deleteMethod = {
@@ -36,12 +45,54 @@ const Deck = () => {
       },
     };
 
-    const url = `/api/deck/${deckId}`;
-
     fetch(url, deleteMethod)
       .then((response) => response.json)
       .catch((err) => console.log(err));
     router.push("/deck");
+  };
+
+  const handleAddToDeck = (id: number) => {
+    showErrorAlreadyExists(false);
+
+    characters.forEach((char: any) => {
+      if (char.character.id === id) {
+        showErrorAlreadyExists(true);
+      }
+    });
+
+    if (!errorAlreadyExists) {
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          action: "add",
+          characters: [id],
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const handleDeleteFromDeck = (id: number) => {
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        characters: [id],
+        action: "delete",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   if (isLoading) return <h2>Loading...</h2>;
@@ -52,17 +103,24 @@ const Deck = () => {
       <h1>{deck.name}</h1>
 
       <h3>Add your character</h3>
+      {allCharacters.map((char: any) => {
+        return (
+          <div key={char.id} onClick={() => handleAddToDeck(char.id)}>
+            {char.name}
+          </div>
+        );
+      })}
 
       <div>
-        <>
-          {characters.map((character: any) => {
-            return (
-              <div key={character.id}>
-                <h4>{character.name}</h4>
-              </div>
-            );
-          })}
-        </>
+        {characters.map((character: any) => {
+          return (
+            <div key={character.characterId}>
+              <h4 onClick={() => handleDeleteFromDeck(character.characterId)}>
+                {character.character.name}
+              </h4>
+            </div>
+          );
+        })}
       </div>
 
       <button onClick={() => handleDeleteDeck()}>Delete this deck</button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,7 @@
-import Head from "next/head";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Star Wars Decks</title>
-        <meta name="description" content="Create your own Star Wars deck" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-    </>
-  );
+  return <></>;
 }
