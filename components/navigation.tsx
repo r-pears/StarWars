44080dc6c4d83eee8a9ee60228bcf95fd2eedf6c
Ryan Pears <ryjen.pears@gmail.com>
2023-07-17 08:19:35 +0200

    style added

---
+++ components/navigation.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import navigation from "./navigation.module.css";
+
+const Navigation = ({ currentPath }: { currentPath: string }) => {
+  console.log(currentPath);
+  return (
+    <div className={navigation.container}>
+      <Link
+        className={
+          navigation.link +
+          " " +
+          (currentPath === "/deck" ? navigation.active : "")
+        }
+        href={`/deck`}
+      >
+        Decks
+      </Link>
+      <Link
+        className={
+          navigation.link +
+          " " +
+          (currentPath === "/deck/new" ? navigation.active : "")
+        }
+        href={`/deck/new`}
+      >
+        Create deck
+      </Link>
+    </div>
+  );
+};
+
+export default Navigation;
