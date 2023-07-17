681f8eb4f3ed23e5f07d79a3d1bfd7637bca1251
Ryan Pears <ryjen.pears@gmail.com>
2023-07-17 07:52:54 +0200

    updated styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import swIcon from "../public/sw.svg";
+import home from "./index.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className={home.container}>
+      <h1 className={home.title}>
+        Create your own deck of Star Wars characters
+      </h1>
+      <Image src={swIcon} alt="Star Wars logo" />
+    </div>
+  );
 }
